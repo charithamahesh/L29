725d5c1e4619bd68a49b73bd14e3fe8db44e1052
CHARITHA  M <charitha.m@s.amity.edu>
2025-02-19 19:44:31 +0530

    add two numbers

---
+++ add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	float num1,num2,sum;
+
+	printf("Enter two numbers");
+	scanf("%f %f",&num1,&num2);
+
+	sum=num1+num2;
+
+	printf("sum:%.2f\n",sum);
+
+	return 0;
+
+}
+

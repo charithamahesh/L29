8daacdb8f9dd89a036abaf80eef45f993f6654fe
CHARITHA  M <charitha.m@s.amity.edu>
2025-02-19 19:51:13 +0530

    quotient of two numbers

--- add.c
+++ add.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 int main() {
-	float num1,num2,sum,difference,product;
+	float num1,num2,sum,difference,product,quotient;
 
 	printf("Enter two numbers");
 	scanf("%f %f",&num1,&num2);
 
 	sum=num1+num2;
 	difference=num1-num2;
-        product=num1*num2
+        product=num1*num2;
+	quotient=num1/num2;
 
 	printf("sum:%.2f\n",sum);
         printf("difference:%.2f\n",difference);
 	printf("product:%.2f\n",product);
+	printf("quotient:%.2f\n",quotient);
 
 
 	return 0;

d1fe16195eb7ce2b7f5467d8b2b5eace3d09d884
CHARITHA  M <charitha.m@s.amity.edu>
2025-02-19 19:49:03 +0530

    product of two numbers

--- add.c
+++ add.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 int main() {
-	float num1,num2,sum,difference;
+	float num1,num2,sum,difference,product;
 
 	printf("Enter two numbers");
 	scanf("%f %f",&num1,&num2);
 
 	sum=num1+num2;
 	difference=num1-num2;
+        product=num1*num2
 
 	printf("sum:%.2f\n",sum);
         printf("difference:%.2f\n",difference);
+	printf("product:%.2f\n",product);
 
 
 	return 0;

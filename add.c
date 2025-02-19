dfe41539b74d01a191b45c34c215a5f3459f5c41
CHARITHA  M <charitha.m@s.amity.edu>
2025-02-19 19:46:50 +0530

    difference of two numbers

--- add.c
+++ add.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 int main() {
-	float num1,num2,sum;
+	float num1,num2,sum,difference;
 
 	printf("Enter two numbers");
 	scanf("%f %f",&num1,&num2);
 
 	sum=num1+num2;
+	difference=num1-num2;
 
 	printf("sum:%.2f\n",sum);
+        printf("difference:%.2f\n",difference);
+
 
 	return 0;
 

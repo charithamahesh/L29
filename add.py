54788dc09a07d5ab4d13e4e81b14989868649d72
CHARITHA  M <charitha.m@s.amity.edu>
2025-02-20 13:16:50 +0530

    sub

--- add.py
+++ add.py
@@ -1,13 +1,15 @@
 #function to perform basic arithmetic operations
 def calculate(a,b):
     add=a+b
+    sub=a-b
 
-    return add
+    return add,sub
 
 #taking input from the user
 num1=float(input("enter the first number:"))
 num2=float(input("enter the second number:"))
 
-add=calculate(num1,num2)
+add,sub=calculate(num1,num2)
 
 print(f"add:{add}")
+print(f"sub:{sub}")

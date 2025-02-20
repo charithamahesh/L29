b3c5e0ad7915af7f0e9c75757e08f8cc2e6b38ea
CHARITHA  M <charitha.m@s.amity.edu>
2025-02-20 13:19:49 +0530

    multiply

--- add.py
+++ add.py
@@ -2,14 +2,16 @@
 def calculate(a,b):
     add=a+b
     sub=a-b
+    multiply=a*b
 
-    return add,sub
+    return add,sub,multiply
 
 #taking input from the user
 num1=float(input("enter the first number:"))
 num2=float(input("enter the second number:"))
 
-add,sub=calculate(num1,num2)
+add,sub,multiply=calculate(num1,num2)
 
 print(f"add:{add}")
 print(f"sub:{sub}")
+print(f"multiply:{multiply}")

8bd35ee89fafbe35c6a4ab60e71b714bea805c44
CHARITHA  M <charitha.m@s.amity.edu>
2025-02-20 13:21:53 +0530

    divide

--- add.py
+++ add.py
@@ -3,15 +3,17 @@ def calculate(a,b):
     add=a+b
     sub=a-b
     multiply=a*b
+    divide=a/b
 
-    return add,sub,multiply
+    return add,sub,multiply,divide
 
 #taking input from the user
 num1=float(input("enter the first number:"))
 num2=float(input("enter the second number:"))
 
-add,sub,multiply=calculate(num1,num2)
+add,sub,multiply,divide=calculate(num1,num2)
 
 print(f"add:{add}")
 print(f"sub:{sub}")
 print(f"multiply:{multiply}")
+print(f"divide:{divide}")

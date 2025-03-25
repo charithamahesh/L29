2940177367883aa6220c97dccc72b551b0ad53c6
CHARITHA  M <charitha.m@s.amity.edu>
2025-03-25 17:59:13 +0530

    div

--- switchcase.c
+++ switchcase.c
@@ -2,7 +2,7 @@
 int main () {
 	int num1,num2,choice;
 	printff("simple calculator\n");
-	printf("1.addition\n");
+	printf("1.addition,2.subtraction,3.multiplication,4.division\n");
 	printf("enter your choice:");
 	scanf("%d",&choice);
 
@@ -15,6 +15,12 @@ case1:
 		break;
 case2:
 		printf("result:%d-%d=%d\n",num1,num2,num1-num2);
+		break;
+case3:
+		printf("result:%d*%d=%d\n",num1,num2,num1*num2);
+		break;
+case4:
+		printf("result:%.2f/%.2f=%.2f\n",num1,num2,num1/num2);
 		break;
 		default:
 		printf("invalid choice! please enter a number between 1 and 4.\n");

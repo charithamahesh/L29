082825c62eaf6567723a012d56ca2cacf0fecd30
CHARITHA  M <charitha.m@s.amity.edu>
2025-03-26 18:13:04 +0530

    case2

--- branches.c
+++ branches.c
@@ -4,12 +4,16 @@ int main () {
 
 	printf("select a B.tech branch:\n");
 	printf("1.Computer science and Engineering\n");
+        printf("2.Artificial Intelligance\n");
 
 	scanf("%d",&choice);
 
 	switch {
 case1:
 		printf("you selected Computer Science and Engineering.\n");
+		break;
+case2:
+		printf("you selected Artificial Intelligance.\n");
 		break;
 		default:
 		printf("invalid choice! please select a number between 1 and 4.\n");
